69921d189aa611451baaf20bf53853b31a2fea9d
cristianPluis <cristianpluis@gmail.com>
2022-03-21 20:08:48 -0300

    crecion nav

---
+++ src/components/NavBar.js
@@ -0,0 +1,29 @@
+import React from "react"
+import '../NavBar.css'
+import logo from '../imagenes/logo.png'
+import ShoppingCartIcon from '@mui/icons-material/ShoppingCart';
+
+
+function NavBar() {
+  return (
+    <div className="nav">
+      <img src={logo}></img>
+      <h1 className="">Titulo E-commerce</h1>
+      <nav>
+        <a href="#">link 1</a>
+        <a href="#">link 2</a>
+        <a href="#">link 3</a>
+        <a href="#">link 4</a>
+        <a href="#">link 5</a>
+        <a href="#">link 6</a>
+      </nav>
+      <div className="cart-container">
+            <a href="#">
+                <ShoppingCartIcon className="carro" />
+            </a>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
